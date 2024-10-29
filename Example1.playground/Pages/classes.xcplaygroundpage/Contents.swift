66ecf7aa53215731f70ce952c63952539a2f3c35
embaabu <edwin.mbaabu@techconsulting.tech>
2024-10-28 22:28:15 -0400

    example of a class in swift

--- Example1.playground/Pages/classes.xcplaygroundpage/Contents.swift
+++ Example1.playground/Pages/classes.xcplaygroundpage/Contents.swift
@@ -1,1 +1,65 @@
 
+
+class Car {
+    // properties of a car
+    var make: String
+    var model: String
+    var year: Int
+    var mileage: Double
+    
+    //initializer
+    
+    init(make: String, model: String, year: Int, mileage: Double) {
+        self.make = make
+        self.model = model
+        self.year = year
+        self.mileage = mileage
+    }
+    
+    //function to display car details
+    func displayDetails(){
+        print("car details: \(year) \(make) \(model) with \(mileage) miles")
+    }
+    //method to drive the car
+    func drive(miles: Double){
+        mileage += miles
+        print("Driving \(miles) miles...")
+    }
+}
+
+//subclass inherits from car
+class ElectricCar: Car{
+    //additional properties
+    var batteryLevel: Double
+    
+    init(make: String, model: String, year: Int, mileage: Double, batteryLevel: Double) {
+        self.batteryLevel = batteryLevel
+        
+        super.init(make: make, model: model, year: year, mileage: mileage)
+    }
+    //overiding the drive method
+    override func drive(miles: Double) {
+        if batteryLevel > 0 {
+            super.drive(miles: miles)
+            batteryLevel -= miles * 0.2
+            print("Battery level is \(batteryLevel)")
+        }else{
+            print("Battery is empty! please recharge.")
+        }
+    }
+    
+}
+// Creating an instance of Car
+let myCar = Car(make: "Toyota", model: "Camry", year: 2020, mileage: 15000)
+myCar.displayDetails()  // Output: Car Details: 2020 Toyota Camry with 15000.0 miles
+myCar.drive(miles: 100) // Output: Driving 100 miles...
+myCar.displayDetails()  // Output: Car Details: 2020 Toyota Camry with 15100.0 miles
+
+// Creating an instance of ElectricCar
+let myElectricCar = ElectricCar(make: "Tesla", model: "Model 3", year: 2021, mileage: 5000, batteryLevel: 100.0)
+myElectricCar.displayDetails()  // Output: Car Details: 2021 Tesla Model 3 with 5000.0 miles
+myElectricCar.drive(miles: 50)   // Output: Driving 50 miles... Battery level: 90.0
+myElectricCar.drive(miles: 500)  // Output: Driving 500 miles... Battery level: 50.0
+myElectricCar.drive(miles: 300)  // Output: Driving 300 miles... Battery level: 30.0
+myElectricCar.drive(miles: 200)  // Output: Driving 200 miles... Battery level: 10.0
+myElectricCar.drive(miles: 100)  // Output: Driving 100 miles... Battery is empty! Please recharge.
